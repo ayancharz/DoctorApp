6e7850f70fbe6544a11be80d2f79121def34ff05
ayanoptumsoft <ayan@optumsoft.com>
2017-02-19 23:38:15 +0530

    19th feb

--- app/src/main/java/com/example/ayanshah/docinfo/DBinfo.java
+++ app/src/main/java/com/example/ayanshah/docinfo/DBinfo.java
@@ -6,9 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
-import java.util.ArrayList;
-
 /**
  * Created by Ayan Shah on 1/23/2017.
  */
